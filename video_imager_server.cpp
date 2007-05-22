90cab70d88f0782d057d45d66c618d97534f9efc
Russell Taylor <taylorr@cs.unc.edu>
2007-05-22 17:03:46 +0000

    2007-05-22  Russell M. Taylor II  <taylorr@cs.unc.edu>
    	* video_imager_server.cpp : Changed the version number because I
    		recompiled to make a new server.  For some reason, the
    		Roper server on version 2.06 was not accepting connections
    		on its VRPN port.  Recompiling fixed it.  No idea why.

--- video_imager_server.cpp
+++ video_imager_server.cpp
@@ -15,7 +15,7 @@
 #endif
 
 const int MAJOR_VERSION = 2;
-const int MINOR_VERSION = 6;
+const int MINOR_VERSION = 7;
 
 //-----------------------------------------------------------------
 // This section contains code to initialize the camera and read its
