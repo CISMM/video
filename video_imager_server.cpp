61ac7819f0e1f79f6ae6cea814566f8f4b4d83b8
Russell Taylor <taylorr@cs.unc.edu>
2008-01-09 21:46:17 +0000

    2008-01-09  Russell M. Taylor II  <taylorr@cs.unc.edu>
    	* video_imager_server.cpp : Version 3.0.  Runs a separate thread
    		for a video imager server on a local loopback (port 9999).
    		In the main thread, it runs an imager forwarder on port 3883
    		so that clients can connect and preview as well as request
    		full-rate logging.

--- video_imager_server.cpp
+++ video_imager_server.cpp
@@ -1,3 +1,7 @@
+// Need this here to keep the DirectShow SDK from causing sprintf() to be
+// undefined.
+#define NO_DSHOW_STRSAFE
+
 #include  <math.h>
 #include  <stdlib.h>
 #include  <signal.h>
@@ -5,6 +9,7 @@
 #include  <stdio.h>
 #include  <vrpn_Connection.h>
 #include  <vrpn_Imager.h>
+#include  <vrpn_Imager_Stream_Buffer.h>
 #include  "directx_camera_server.h"
 
 #ifndef	DIRECTX_VIDEO_ONLY
@@ -14,8 +19,40 @@
 #include  "cooke_server.h"
 #endif
 
-const int MAJOR_VERSION = 2;
-const int MINOR_VERSION = 9;
+const int MAJOR_VERSION = 3;
+const int MINOR_VERSION = 0;
+
+//-----------------------------------------------------------------
+// g_done gets set when the user presses ^C to exit the program.
+
+static	bool  g_done = false;	//< Becomes true when time to exit
+
+// WARNING: On Windows systems, this handler is called in a separate
+// thread from the main program (this differs from Unix).  To avoid all
+// sorts of chaos as the main program continues to handle packets, we
+// set a done flag here and let the main program shut down in its own
+// and do all of the stuff we used to do in this handler.
+// This flag actually stops two threads: the imager-server thread and
+// the main-program thread.  The main-program thread runs the imager
+// forwarder (which actually has its own sub-thread, but that death
+// is handled internally).
+
+void handle_cntl_c(int) {
+  g_done = true;
+}
+
+void  Usage(const char *s)
+{
+  fprintf(stderr,"Usage: %s [-expose msecs] [-bin count] [-res x y] [-swap_edt] [devicename [devicenum]]\n",s);
+  fprintf(stderr,"       -expose: Exposure time in milliseconds (default 250)\n");
+  fprintf(stderr,"       -bin: How many pixels to average in x and y (default 1)\n");
+  fprintf(stderr,"       -res: Resolution in x and y (default 320 200)\n");
+  fprintf(stderr,"       -swap_edt: Swap lines to fix a bug in the EDT camera\n");
+  fprintf(stderr,"       devicename: roper, edt, cooke, diaginc, or directx (default is directx)\n");
+  fprintf(stderr,"       devicenum: Which (starting with 1) if there are multiple (default 1)\n");
+  fprintf(stderr,"       logfilename: Name of file to store outgoing log in (default NULL)\n");
+  exit(-1);
+}
 
 //-----------------------------------------------------------------
 // This section contains code to initialize the camera and read its
@@ -108,15 +145,45 @@ void  teardown_camera_code(void)
 //-----------------------------------------------------------------
 // This section contains code that does what the server should do
 
-vrpn_Connection               *svrcon;	//< Connection for server to talk on
+// Handled on a separate thread that continually runs the camera
+vrpn_Thread                   *svrthread;//< Thread to handle mainlooping the imager server.
+vrpn_Connection               *svrcon;  //< Local loopback connection for imager server
 vrpn_Imager_Server	      *svr;	//< Image server to be used to send
 int			      svrchan;	//< Server channel index for image data
 
+// Handled in the main thread.
+vrpn_Connection               *strcon;	//< Connection for stream server to talk on
+vrpn_Imager_Stream_Buffer     *str;     //< Stream server that listens to the imager server
+
+// The function that is called to become the logging thread.  It is passed
+// a pointer to "this" so that it can acces the object that created it.
+// The static function basically just unpacks the "this" pointer and
+// then calls the non-static function.
+void imager_server_thread_func(vrpn_ThreadData &threadData)
+{
+  // Mainloop the server and its connection until it is time to quit.
+  while (!g_done) {
+    // Setting the min to be larger than the max means "the whole image"
+    g_camera->read_image_to_memory(1,0,1,0,g_exposure);
+    if (!g_camera->send_vrpn_image(svr,svrcon,g_exposure,svrchan, g_numchannels)) {
+      fprintf(stderr, "Could not send VRPN frame\n");
+    }
+
+    svr->mainloop();
+    svrcon->mainloop();
+    svrcon->save_log_so_far();
+  }
+  if (svr) { delete svr; svr = NULL; };
+  if (svrcon) { delete svrcon; svrcon = NULL; };
+  teardown_camera_code();
+}
+
 bool  init_server_code(const char *outgoing_logfile_name, bool do_color)
 {
-  const int PORT = vrpn_DEFAULT_LISTEN_PORT_NO;
-  if ( (svrcon = new vrpn_Connection(PORT, NULL, outgoing_logfile_name)) == NULL) {
-    fprintf(stderr, "Could not open server connection\n");
+  const int svrPORT = 9999;
+  const int strPORT = vrpn_DEFAULT_LISTEN_PORT_NO;
+  if ( (svrcon = new vrpn_Connection(svrPORT, NULL, outgoing_logfile_name)) == NULL) {
+    fprintf(stderr, "Could not open imager server connection\n");
     return false;
   }
   if ( (svr = new vrpn_Imager_Server("TestImage", svrcon,
@@ -145,44 +212,40 @@ bool  init_server_code(const char *outgoing_logfile_name, bool do_color)
       return false;
     }
   }
-  printf("Waiting for video connections on %d\n", PORT);
+  vrpn_ThreadData td;
+  td.pvUD = NULL;
+  svrthread = new vrpn_Thread(imager_server_thread_func, td);
+  if (svrthread == NULL) {
+    fprintf(stderr,"Can't create server thread\n");
+    return false;
+  }
+  if (!svrthread->go()) {
+    fprintf(stderr,"Can't start server thread\n");
+    return false;
+  }
+  printf("Local loopback server thread on %d\n", svrPORT);
+
+  // Now handle the main thread's code, which will run an
+  // imager forwarder.
+  if ( (strcon = new vrpn_Connection(strPORT)) == NULL) {
+    fprintf(stderr, "Could not open imager stream buffer connection\n");
+    return false;
+  }
+  char  svrname[1024];
+  sprintf(svrname,"TestImage@localhost:%d", svrPORT);
+  if ( (str = new vrpn_Imager_Stream_Buffer("TestImage", svrname, strcon)) == NULL) {
+    fprintf(stderr, "Could not open imager stream buffer\n");
+    return false;
+  }
+  printf("Waiting for video connections on %d\n", strPORT);
 
   return true;
 }
 
 void  teardown_server_code(void)
 {
-  if (svr) { delete svr; svr = NULL; };
-  if (svrcon) { delete svrcon; svrcon = NULL; };
-}
-
-//-----------------------------------------------------------------
-// g_done gets set when the user presses ^C to exit the program.
-
-static	bool  g_done = false;	//< Becomes true when time to exit
-
-// WARNING: On Windows systems, this handler is called in a separate
-// thread from the main program (this differs from Unix).  To avoid all
-// sorts of chaos as the main program continues to handle packets, we
-// set a done flag here and let the main program shut down in its own
-// thread by calling shutdown() to do all of the stuff we used to do in
-// this handler.
-
-void handle_cntl_c(int) {
-  g_done = true;
-}
-
-void  Usage(const char *s)
-{
-  fprintf(stderr,"Usage: %s [-expose msecs] [-bin count] [-res x y] [-swap_edt] [devicename [devicenum]]\n",s);
-  fprintf(stderr,"       -expose: Exposure time in milliseconds (default 250)\n");
-  fprintf(stderr,"       -bin: How many pixels to average in x and y (default 1)\n");
-  fprintf(stderr,"       -res: Resolution in x and y (default 320 200)\n");
-  fprintf(stderr,"       -swap_edt: Swap lines to fix a bug in the EDT camera\n");
-  fprintf(stderr,"       devicename: roper, edt, cooke, diaginc, or directx (default is directx)\n");
-  fprintf(stderr,"       devicenum: Which (starting with 1) if there are multiple (default 1)\n");
-  fprintf(stderr,"       logfilename: Name of file to store outgoing log in (default NULL)\n");
-  exit(-1);
+  if (str) { delete str; str = NULL; };
+  if (strcon) { delete strcon; strcon = NULL; };
 }
 
 //-----------------------------------------------------------------
@@ -257,21 +320,15 @@ int main(int argc, char *argv[])
   if (!init_server_code(logfilename, (g_numchannels > 1) )) { return -1; }
 
   while (!g_done) {
-    // Setting the min to be larger than the max means "the whole image"
-    g_camera->read_image_to_memory(1,0,1,0,g_exposure);
-    if (!g_camera->send_vrpn_image(svr,svrcon,g_exposure,svrchan, g_numchannels)) {
-      fprintf(stderr, "Could not send VRPN frame\n");
-    }
-    svr->mainloop();
-    svrcon->mainloop();
-    svrcon->save_log_so_far();
-    // We do want this process to eat a whole processor, so
-    // don't sleep.
-//    vrpn_SleepMsecs(1);
+    str->mainloop();
+    strcon->mainloop();
+
+    // Sleep to avoid eating the whole processor.  The camera-watching thread
+    // does not sleep, so that it gets frames as fast as it can.
+    vrpn_SleepMsecs(1);
   }
 
   printf("Deleting camera and connection objects\n");
   teardown_server_code();
-  teardown_camera_code();
   return 0;
 }
