e3b465f0438fdf8370d5ebdfea00bb78f9f2189c
Russell Taylor <taylorr@cs.unc.edu>
2008-11-22 14:20:10 +0000

    Adds an installer that does not require NSRG runtime to run the resulting program.

--- cismm_video_optimizer/cismm_video_optimizer.bat
+++ cismm_video_optimizer/cismm_video_optimizer.bat
@@ -1,5 +1,5 @@
 set PATH=C:\NSRG\external\pc_win32\bin\ImageMagick-5.5.7-Q16;C:\NSRG\external\pc_win32\bin\
-set TCL_LIBRARY=C:/nsrg/external/pc_win32/lib/tcl8.3/
+set TCL_LIBRARY=C:/nsrg/external/pc_win32/lib/tcl8.3/
 set TK_LIBRARY=C:/nsrg/external/pc_win32/lib/tk8.3/
 .\cismm_video_optimizer.exe %1
 if not errorlevel 0 pause
