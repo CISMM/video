727e001b0f4f6f7e9ba48bfdc76ebe11e19d9e96
Russell Taylor <taylorr@cs.unc.edu>
2010-05-18 12:23:48 +0000

    Adds more command-line arguments.

--- video_imager_server/video_imager_server.bat
+++ video_imager_server/video_imager_server.bat
@@ -1,4 +1,4 @@
 set PATH=C:\NSRG\external\pc_win32\bin\ImageMagick-5.5.7-Q16;C:\NSRG\external\pc_win32\bin\
 set TCL_LIBRARY=C:/nsrg/external/pc_win32/lib/tcl8.3/set TK_LIBRARY=C:/nsrg/external/pc_win32/lib/tk8.3/
-.\video_imager_server.exe %1 %2 %3
+.\video_imager_server.exe %1 %2 %3 %4 %5 %6 %7 %8 %9 %10
 if not errorlevel 0 pause
