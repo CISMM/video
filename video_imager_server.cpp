f88533f6c5d91aede733b2deda30112f8fd58e3d
Russell Taylor <taylorr@cs.unc.edu>
2011-05-20 11:48:21 -0400

    Version 3.09 includes a new testimager client.  Also includes all changes since last time (Panoptes driver for Point Gray camera).  Inclues an installer project file.

--- video_imager_server.cpp
+++ video_imager_server.cpp
@@ -21,7 +21,7 @@
 #endif
 
 const int MAJOR_VERSION = 3;
-const int MINOR_VERSION = 8;
+const int MINOR_VERSION = 9;
 
 //-----------------------------------------------------------------
 // g_done gets set when the user presses ^C to exit the program.
