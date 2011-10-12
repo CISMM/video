e2626d25a66f04be02ff764b66916a321eb6e2e7
Russell M. Taylor II <taylorr@cs.unc.edu>
2011-10-12 14:23:56 -0400

    Version 06.08 of Video Spot Tracker, with previous bug fixes.

--- video_spot_tracker.cpp
+++ video_spot_tracker.cpp
@@ -79,7 +79,7 @@ const double M_PI = 2*asin(1.0);
 
 //--------------------------------------------------------------------------
 // Version string for this program
-const char *Version_string = "06.07";
+const char *Version_string = "06.08";
 
 //--------------------------------------------------------------------------
 // Global constants
