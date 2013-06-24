a2f471e102ec3136087b9ed77576f336deb7fcc6
Patrick Hahn <phahn@cs.unc.edu>
2013-06-24 11:31:28 -0400

    Added feature to include a moving Gaussian bead to the video

--- make_compression_video.cpp
+++ make_compression_video.cpp
@@ -8,13 +8,14 @@ void Usage (const char * s)
 {
   fprintf(stderr,"Usage: %s [-v] [-frames FRAMES]\n", s);
   fprintf(stderr,"       [-resolution WIDTH HEIGHT]\n");
+  fprintf(stderr,"       [-bead MAX STD] [-motion RAD SPD]\n");
   fprintf(stderr,"       [-noise F V] [BASENAME]\n");
   fprintf(stderr,"     -v: Verbose mode\n");
   fprintf(stderr,"     -frames: The number of output images (default 120)\n");
-  fprintf(stderr,"     -oversampling: How many times oversampling in X and Y (default 100)\n");
   fprintf(stderr,"     -resolution: Specify the image size in X and Y (default 101 101)\n");
+  fprintf(stderr,"     -bead: Add Gaussian bead with max value MAX and standard deviation STD\n");
+  fprintf(stderr,"     -motion: Bead moves in circular motion with radius RAD at speed SPD (degrees/second)\n");
   fprintf(stderr,"     -noise: Add Poisson noise with fraction of proportionality F and Gaussian noise of variance V\n");
-
   fprintf(stderr,"     BASENAME: Base name for the output file (default ./test_video if not specified)\n");
   exit(0);
 }
@@ -31,14 +32,17 @@ public:
   // spots in the specified locations (may be subpixel) with respective
   // radii and intensities, as listed in the spot vector, s.
   compression_test_image(int minx = 0, int maxx = 255, int miny = 0, 
-		 int maxy = 255, double gaussian_var = 0, double poisson_frac = 0);
+		 int maxy = 255, double gaussian_var = 0, double poisson_frac = 0,
+		 double bead_val = 0, double bead_std = 0, double x = 0, double y = 0);
 };
 
 compression_test_image::compression_test_image(int minx, int maxx, int miny, int maxy,
-		double gaussian_var, double poisson_frac) :
+		double gaussian_var, double poisson_frac, double bead_val, double bead_std,
+		double x, double y) :
   double_image(minx, maxx, miny, maxy)
 {
   int i,j, index;
+  double summedvolume = bead_val * bead_std * bead_std * 2 * M_PI;
 
   // Fill in the base intensity of each pixel.
   for (i = _minx; i <= _maxx; i++) {
@@ -46,6 +50,29 @@ compression_test_image::compression_test_image(int minx, int maxx, int miny, int
 		double intensity = (i / double(maxx)) * 65535;
 		write_pixel_nocheck(i, j, intensity);
 	}
+  }
+
+  // Fill in the Gaussian intensity plus background.
+  // Note that the area under the curve for the unit Gaussian is 1; we
+  // multiply by the summedvolume (which needs to be the sum above or
+  // below the background) to produce an overall volume matching the
+  // one requested.
+#ifdef	DEBUG
+  printf("Gaussian_image::recompute(): Making Gaussian of standard deviation %lg, background %lg, volume %lg\n", std_dev, background, summedvolume);
+#endif
+  for (i = (int)floor(x - (3.5*bead_std)); i <= (int)ceil(x + (3.5*bead_std)); i++) {
+	for (j = (int)floor(y - (3.5*bead_std)); j <= (int)ceil(y + (3.5*bead_std)); j++) {
+      double x0 = (i - x) - 0.5;    // The left edge of the pixel in Gaussian space
+      double x1 = x0 + 1;                 // The right edge of the pixel in Gaussian space
+      double y0 = (j - y) - 0.5;    // The bottom edge of the pixel in Gaussian space
+      double y1 = y0 + 1;                 // The top edge of the pixel in Gaussian space
+      if (find_index(i,j,index)) {
+        // For this call to ComputeGaussianVolume, we assume 1-meter pixels.
+        // This makes std dev and other be in pixel units without conversion.
+	    _image[index] = _image[index] + ComputeGaussianVolume(summedvolume, bead_std,
+                        x0,x1, y0,y1, 100);
+      }
+    }
   }
 
   //------------------------------------------------------------------------
@@ -82,7 +109,13 @@ int main(int argc, char *argv[])
   unsigned frames = 120;              // How many frames to write
   double gaussian_mean = 0;			  // Mean value of Gaussian noise
   double gaussian_var = 0;			  // Variance of Gaussian noise
+  double bead_val = 0;				  // Max value of Gaussian bead
+  double bead_std = 0;				  // Standard deviation of Gaussian bead
+  double start_x = 0;				  // Initial x-coordinate for bead position
+  double start_y = 0;				  // Initial y-coordinate for bead position
   double poisson_frac = 0;			  // Fraction of proportionality for Poisson noise
+  double motion_rad = 0;			  // Radius of circular motion
+  double motion_spd = 0;			  // Speed of movement around circle in degrees/second
   const char *basename = "test_video";
   bool verbose = false;               // Print out info along the way?
 
@@ -98,6 +131,20 @@ int main(int argc, char *argv[])
 	  width = atoi(argv[i]);
           if (++i > argc) { Usage(argv[0]); }
 	  height = atoi(argv[i]);
+	} else if (!strncmp(argv[i], "-bead", strlen("-bead"))) {
+          if (++i > argc) { Usage(argv[0]); }
+	  bead_val = 65535* atof(argv[i]);
+          if (++i > argc) { Usage(argv[0]); }
+	  bead_std = atof(argv[i]);
+		  if (++i > argc) { Usage(argv[0]); }
+	  start_x = atof(argv[i]);
+          if (++i > argc) { Usage(argv[0]); }
+	  start_y = atof(argv[i]);
+	} else if (!strncmp(argv[i], "-motion", strlen("-motion"))) {
+          if (++i > argc) { Usage(argv[0]); }
+	  motion_rad = atof(argv[i]);
+          if (++i > argc) { Usage(argv[0]); }
+	  motion_spd = atof(argv[i]);
     } else if (!strncmp(argv[i], "-noise", strlen("-noise"))) {
           if (++i > argc) { Usage(argv[0]); }
 	  poisson_frac = atof(argv[i]);
@@ -132,10 +179,22 @@ int main(int argc, char *argv[])
   // Generate images with specified noise and write them to files.
 
   unsigned frame;
-  for (frame = 0; frame < frames; frame++) {    
+  for (frame = 0; frame < frames; frame++) { 
+	// Find out where bead should be
+	double x = start_x, y = start_y;
+	// Subtracting 1 from the cos() value so that we start at the
+    // correct location, not one radius away from it.
+    x = start_x + motion_rad * (cos(frame * motion_spd * M_PI/180) - 1);
+    y = start_y + motion_rad * sin(frame * motion_spd * M_PI/180);
+
+	// Flip the image in Y because the write function is going to flip it again
+    // for us before saving the image.
+    double flip_y = (height - 1) - y;
+
     // Make a new image
     image_wrapper *cur_image;
-	cur_image = new compression_test_image(0, width-1, 0, height-1, gaussian_var, poisson_frac);    
+	cur_image = new compression_test_image(0, width-1, 0, height-1, gaussian_var, poisson_frac, 
+											bead_val, bead_std, x, flip_y);    
 
     // Write the image to file whose name is the base name with
     // the frame number and .tif added.
